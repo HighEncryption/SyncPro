HighEncryption/SyncPro

commit e8329f8d365fd0be58add16ec6834deaeb7f6706

Performance: When analying folders for changes, don't query the DB for entries when the parent is new.

SyncPro.Core/Runtime/SyncAnalyzer.cs
@@ -365,9 +365,15 @@ namespace SyncPro.Runtime
             Logger.Verbose("Found {0} child items from adapter.", adapterChildren.Count);
 
             // Get the children (files and directories) for a particular directory as identified by 'entry'.
-            List<SyncEntry> logicalChildren = db.Entries.Include(e => e.AdapterEntries).Where(e => e.ParentId == logicalParent.Id).ToList();
+            // Performance Note: Check if the logical parent is a new item. If so, then any of the children will
+            // be new as well, so don't query the DB for them.
+            List<SyncEntry> logicalChildren = null;
+            if (logicalParent.UpdateInfo != null && !logicalParent.UpdateInfo.HasSyncEntryFlag(SyncEntryChangedFlags.IsNew))
+            {
+                logicalChildren = db.Entries.Include(e => e.AdapterEntries).Where(e => e.ParentId == logicalParent.Id).ToList();
+            }
 
-            Logger.Verbose("Found {0} child items from database.", logicalChildren.Count);
+            Logger.Verbose("Found {0} child items from database.", logicalChildren?.Count);
 
             // Loop through each of the items return by the adapter (eg files on disk)
             foreach (IAdapterItem adapterChild in adapterChildren)
@@ -392,7 +398,7 @@ namespace SyncPro.Runtime
                 Logger.Verbose("Examining adapter child item {0}", adapterChild.FullName);
 
                 // First check if there is an entry that matches the unique ID of the item
-                SyncEntry logicalChild = logicalChildren.FirstOrDefault(c => c.HasUniqueId(adapterChild.UniqueId));
+                SyncEntry logicalChild = logicalChildren?.FirstOrDefault(c => c.HasUniqueId(adapterChild.UniqueId));
 
                 // A match was found, so determine
                 if (logicalChild != null)
@@ -475,44 +481,47 @@ namespace SyncPro.Runtime
 
             // Any entries still in the indexChildren list are no longer present in the source adapter and
             // need to be expunged (recursivly).
-            foreach (
-                SyncEntry oldChild in logicalChildren.Where(e => e.State.HasFlag(SyncEntryState.IsDeleted) == false))
+            if (logicalChildren != null)
             {
-                if (this.cancellationToken.IsCancellationRequested)
+                foreach (
+                    SyncEntry oldChild in logicalChildren.Where(e => e.State.HasFlag(SyncEntryState.IsDeleted) == false))
                 {
-                    break;
-                }
+                    if (this.cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
 
-                Logger.Verbose("Child item {0} ({1}) was deleted.", oldChild.Id, oldChild.Name);
+                    Logger.Verbose("Child item {0} ({1}) was deleted.", oldChild.Id, oldChild.Name);
 
-                // Mark the sync entry as 'deleted' by setting the appropriate bit.
-                oldChild.State |= SyncEntryState.IsDeleted;
+                    // Mark the sync entry as 'deleted' by setting the appropriate bit.
+                    oldChild.State |= SyncEntryState.IsDeleted;
 
-                // Set the state to 'Unsynchronized' (so that we know we have pending changes).
-                oldChild.State |= SyncEntryState.NotSynchronized;
+                    // Set the state to 'Unsynchronized' (so that we know we have pending changes).
+                    oldChild.State |= SyncEntryState.NotSynchronized;
 
-                // Create the update info for the new entry
-                oldChild.UpdateInfo = new EntryUpdateInfo(
-                    oldChild,
-                    adapter,
-                    SyncEntryChangedFlags.Deleted,
-                    Path.Combine(relativePath, oldChild.Name));
-
-                //this.RaiseSyncEntryChanged(oldChild, ItemChangeType.Deleted);
-                //this.AnalyzeOnSyncEntryChanged(this, new SyncEntryChangedEventArgs(oldChild.UpdateInfo));
-                this.RaiseChangeDetected(adapter.Configuration.Id, oldChild.UpdateInfo);
-
-                // If the entry we are removing is a directory, we need to also check for subdirectories and files. We can do this by calling 
-                // this method recursivly, and passing null for adapter folder (indicating that is no longer exists according to the 
-                // originating adapter.
-                if (oldChild.Type == SyncEntryType.Directory)
-                {
-                    this.AnalyzeChangesWithoutChangeTracking(
-                        db, 
-                        adapter, 
-                        null, 
-                        oldChild, 
+                    // Create the update info for the new entry
+                    oldChild.UpdateInfo = new EntryUpdateInfo(
+                        oldChild,
+                        adapter,
+                        SyncEntryChangedFlags.Deleted,
                         Path.Combine(relativePath, oldChild.Name));
+
+                    //this.RaiseSyncEntryChanged(oldChild, ItemChangeType.Deleted);
+                    //this.AnalyzeOnSyncEntryChanged(this, new SyncEntryChangedEventArgs(oldChild.UpdateInfo));
+                    this.RaiseChangeDetected(adapter.Configuration.Id, oldChild.UpdateInfo);
+
+                    // If the entry we are removing is a directory, we need to also check for subdirectories and files. We can do this by calling 
+                    // this method recursivly, and passing null for adapter folder (indicating that is no longer exists according to the 
+                    // originating adapter.
+                    if (oldChild.Type == SyncEntryType.Directory)
+                    {
+                        this.AnalyzeChangesWithoutChangeTracking(
+                            db,
+                            adapter,
+                            null,
+                            oldChild,
+                            Path.Combine(relativePath, oldChild.Name));
+                    }
                 }
             }
         }

commit e9bb84477f7cc2ef5673aa0dae9c1fe71230736b

Fixing a bug introduced in e8329f8: Refine the logic used to determine when to query the DB for child entries.

SyncPro.Core/Global.cs
@@ -15,7 +15,7 @@
         }
 
         // Test Hook
-        internal static void Initialize(string root, bool isTestMode = true)
+        internal static void Initialize(string root, bool isTestMode = false)
         {
             Global.AppDataRoot = root;
 

SyncPro.Core/Runtime/AnalyzeAdapterResult.cs
@@ -1,5 +1,6 @@
 namespace SyncPro.Runtime
 {
+    using System;
     using System.Collections.Generic;
 
     public class AnalyzeAdapterResult
@@ -26,6 +27,11 @@ namespace SyncPro.Runtime
         /// will be committed to the adapter.
         /// </remarks>
         public bool ForceCommitChanges { get; set; }
+
+        /// <summary>
+        /// Exception thrown during analysis.
+        /// </summary>
+        public Exception Exception { get; set; }
 
         public AnalyzeAdapterResult()
         {

SyncPro.Core/Runtime/SyncAnalyzer.cs
@@ -112,33 +112,40 @@ namespace SyncPro.Runtime
         {
             Logger.Info("Beginning AnalyzeChangesFromAdapter for adapter " + adapter.Configuration.Id);
 
-            using (SyncDatabase db = this.relationship.GetDatabase())
+            try
             {
-                SyncEntry rootIndexEntry = db.Entries.FirstOrDefault(e => e.Id == adapter.Configuration.RootIndexEntryId);
-                if (adapter.SupportChangeTracking())
+                using (SyncDatabase db = this.relationship.GetDatabase())
                 {
-                    IChangeTracking changeTracking = (IChangeTracking)adapter;
-                    TrackedChange trackedChange = await changeTracking.GetChangesAsync().ConfigureAwait(false);
+                    SyncEntry rootIndexEntry = db.Entries.FirstOrDefault(e => e.Id == adapter.Configuration.RootIndexEntryId);
 
-                    this.analyzeResult.TrackedChanges.Add(adapter, trackedChange);
-                    this.AnalyzeChangesWithChangeTracking(db, adapter, rootIndexEntry);
+                    if (adapter.SupportChangeTracking())
+                    {
+                        IChangeTracking changeTracking = (IChangeTracking)adapter;
+                        TrackedChange trackedChange = await changeTracking.GetChangesAsync().ConfigureAwait(false);
 
-                    Logger.Info("Finished AnalyzeChangesFromAdapter with change tracking for adapter " + adapter.Configuration.Id);
+                        this.analyzeResult.TrackedChanges.Add(adapter, trackedChange);
+                        this.AnalyzeChangesWithChangeTracking(db, adapter, rootIndexEntry);
+
+                        Logger.Info("Finished AnalyzeChangesFromAdapter with change tracking for adapter " + adapter.Configuration.Id);
+                    }
+                    else
+                    {
+                        IAdapterItem rootFolder = adapter.GetRootFolder().Result;
+
+                        this.AnalyzeChangesWithoutChangeTracking(
+                            db,
+                            adapter,
+                            rootFolder,
+                            rootIndexEntry,
+                            string.Empty);
+
+                        Logger.Info("Finished AnalyzeChangesFromAdapter for adapter " + adapter.Configuration.Id);
+                    }
                 }
-                else
-                {
-                    IAdapterItem rootFolder = adapter.GetRootFolder().Result;
-
-                    this.AnalyzeChangesWithoutChangeTracking(
-                        db,
-                        adapter,
-                        rootFolder,
-                        rootIndexEntry,
-                        string.Empty);
-
-                    Logger.Info("Finished AnalyzeChangesFromAdapter for adapter " + adapter.Configuration.Id);
-                }
-
+            }
+            catch (Exception exception)
+            {
+                this.analyzeResult.AdapterResults[adapter.Configuration.Id].Exception = exception;
             }
         }
 
@@ -366,9 +373,16 @@ namespace SyncPro.Runtime
 
             // Get the children (files and directories) for a particular directory as identified by 'entry'.
             // Performance Note: Check if the logical parent is a new item. If so, then any of the children will
-            // be new as well, so don't query the DB for them.
+            // be new as well, so don't query the DB for them. The logic below determines if we need to make this
+            // DB query inverted, as it is easier to read by a human.
+            bool skipChildLookup =
+                logicalParent.State == SyncEntryState.NotSynchronized &&
+                logicalParent.UpdateInfo != null &&
+                logicalParent.UpdateInfo.HasSyncEntryFlag(SyncEntryChangedFlags.IsNew);
+
             List<SyncEntry> logicalChildren = null;
-            if (logicalParent.UpdateInfo != null && !logicalParent.UpdateInfo.HasSyncEntryFlag(SyncEntryChangedFlags.IsNew))
+
+            if (!skipChildLookup)
             {
                 logicalChildren = db.Entries.Include(e => e.AdapterEntries).Where(e => e.ParentId == logicalParent.Id).ToList();
             }
@@ -438,6 +452,9 @@ namespace SyncPro.Runtime
 
                         // Create the update info for the new entry
                         logicalChild.UpdateInfo = new EntryUpdateInfo(logicalChild, adapter, changeFlags, Path.Combine(relativePath, logicalChild.Name));
+
+                        // Set the NotSynchronized flag so that we know this has not yet been committed to the database.
+                        logicalChild.State = SyncEntryState.NotSynchronized;
 
                         // Raise change notification so that the UI can be updated in "real time" rather than waiting for the analyze process to finish.
                         //this.AnalyzeOnSyncEntryChanged(this, new SyncEntryChangedEventArgs(logicalChild.UpdateInfo));