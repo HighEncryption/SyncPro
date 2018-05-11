a9a75cbe67b633f3287e7faf6dc4e5163da9236c
HighEncryption <29420483+HighEncryption@users.noreply.github.com>
2018-05-10 20:46:40 -0700

    Adding strong name signing support #35

--- SyncPro.Core/Properties/AssemblyInfo.cs
+++ SyncPro.Core/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
-[assembly:InternalsVisibleTo("SyncPro.UnitTests")]
+[assembly: InternalsVisibleTo("SyncPro.UnitTests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100f17de2a0db4eea03af9795d6201745c8b5d5e645059d5867ab6ca76a84365689887b21678a4f43249f4aeade07ad129e6f76470b4a118deb55946b68d530cbdc225fa35bc068278755985161afb0aa8fcf2a28a5e8093ea1c9f443023e792cb563abf5bacbbdfaf5a832401a56a1cd55204232f3701893c1bf213864eec5d6e0")]
--- SyncPro.UI/Properties/AssemblyInfo.cs
+++ SyncPro.UI/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: InternalsVisibleTo("OneDriveTokenHelper")]
+[assembly: InternalsVisibleTo("OneDriveTokenHelper,PublicKey=0024000004800000940000000602000000240000525341310004000001000100f17de2a0db4eea03af9795d6201745c8b5d5e645059d5867ab6ca76a84365689887b21678a4f43249f4aeade07ad129e6f76470b4a118deb55946b68d530cbdc225fa35bc068278755985161afb0aa8fcf2a28a5e8093ea1c9f443023e792cb563abf5bacbbdfaf5a832401a56a1cd55204232f3701893c1bf213864eec5d6e0")]
 
 // Version information for an assembly consists of the following four values:
 //
