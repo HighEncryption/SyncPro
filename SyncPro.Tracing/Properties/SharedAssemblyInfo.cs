f69ac3e09615ed31ba4a13d72758d2b04f46e6cb
HighEncryption <29420483+HighEncryption@users.noreply.github.com>
2018-06-09 20:59:35 -0700

    Bump version to 0.0.2

--- SyncPro.Tracing/Properties/SharedAssemblyInfo.cs
+++ SyncPro.Tracing/Properties/SharedAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
